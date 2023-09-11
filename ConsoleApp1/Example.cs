5412008e40761dabad62935c808b1e4092d12159
VELena-73 <vishneva_ea@mail.ru>
2023-09-11 10:41:23 +0800

    Добавление массива и вычисление его суммы

---
+++ ConsoleApp1/Example.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            int[] array= { 1, 2, 3, 4, 5, 6, 7, 12 };
+            int sum=array.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
